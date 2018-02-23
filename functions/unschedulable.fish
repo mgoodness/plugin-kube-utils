2cf9466a85952bc2e5209b8962e5ee2af1709318
Michael Goodness <mgoodness@gmail.com>
2018-02-23 16:57:29 -0600

    Remove unschedulable

--- functions/unschedulable.fish
+++
@@ -1,4 +0,0 @@
-function unschedulable -d 'Lists unschedulable pods'
-  kubectl get pods --all-namespaces -o json | \
-  jq '.items[] | select(.status.conditions[].reason == "Unschedulable") | {"name": .metadata.name, "namespace": .metadata.namespace, "tier": .spec.nodeSelector.tier}'
-end
